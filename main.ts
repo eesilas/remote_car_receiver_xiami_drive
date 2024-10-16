6a5ce06e91c716e614dc9630faa29879d075a416
Learn by Doing <32452061+eesilas@users.noreply.github.com>
2024-10-16 11:39:21 +0800

    add B spin and A+B sing a song.

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 2) {
         xiamiBoard.setIndexColor(1, 0xffff00)
         xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 128)
-        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CCW, 128)
+        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CCW, 72)
         basic.showLeds(`
             . . # . .
             . . . # .
@@ -40,7 +40,7 @@ radio.onReceivedNumber(function (receivedNumber) {
         xiamiBoard.setIndexColor(1, 0x000000)
     } else if (receivedNumber == 4) {
         xiamiBoard.setIndexColor(0, 0xffff00)
-        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 128)
+        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 72)
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 128)
         basic.showLeds(`
             . . # . .
@@ -59,8 +59,17 @@ radio.onReceivedNumber(function (receivedNumber) {
             . # # # .
             . . . . .
             `)
+        blinking()
+    } else if (receivedNumber == 6) {
+        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 255)
+        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CCW, 255)
+    } else if (receivedNumber == 7) {
+        music.setVolume(127)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
+        music.stopAllSounds()
     } else {
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Happy)
+        blinking()
     }
 })
 function blinking () {
@@ -85,5 +94,8 @@ xiamiBoard.motorStop(MOTOR.ALL)
 xiamiBoard.setBrightness(51)
 blinking()
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Heart)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(5000)
 })
